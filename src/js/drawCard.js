4663199125414729d938f03d8c8d167b0b6ef4b7
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-07-13 23:19:55 +0300

    first commit

---
+++ src/js/drawCard.js
@@ -0,0 +1,18 @@
+export default class DrawCard {
+  constructor(container) {
+    this.container = container;
+  }
+
+  static markup(title, type, id) {
+    return `<li class="card" data-type = "${type}" data-id = "${id}">
+      <div class = "card-header">
+          <h4>${title}</h4>
+          <button class="card-delete hidden"><span></span></button>
+      </div>
+  </li>`;
+  }
+
+  bindToDOMCard({ title, type, id }) {
+    this.container.insertAdjacentHTML('beforeend', this.constructor.markup(title, type, id));
+  }
+}
